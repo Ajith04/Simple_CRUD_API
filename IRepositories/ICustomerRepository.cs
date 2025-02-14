63e61779c902de432c38b1086a225fcae12749a7
Zaxsan UCA <zaxsan@unicornconnectedapps.com>
2025-02-14 17:41:18 +0530

    CustomerAPI-Santhosh

---
+++ IRepositories/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using DotNetCRUD.Models;
+
+namespace DotNetCRUD.IRepositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> AddCustomer(Customer customer);
+        Task<Customer> GetCustomer(int id);
+        Task<ICollection<Customer>> GetAllCustomer();
+        Task<Customer> EditCustomer(Customer customer);
+        Task<Customer> DeleteCustomer(int id);
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,83 @@
+﻿using DotNetCRUD.DTO.RequestDTO;
+using DotNetCRUD.DTO.ResponseDTO;
+using DotNetCRUD.IRepositories;
+using DotNetCRUD.IServices;
+using DotNetCRUD.Models;
+using DotNetCRUD.Repositories;
+
+namespace DotNetCRUD.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository  ;
+        }
+
+
+        public async Task<Customer> AddCustomer(CustomerReqDTO customerReq)
+        {
+            var customer = new Customer
+            {
+               CustomerName = customerReq.CustomerName,
+               Age = customerReq.Age,
+               Address = customerReq.Address,
+               PhoneNumber = customerReq.PhoneNumber,
+               EmailAddress = customerReq.EmailAddress,
+
+            };
+            var data = await _customerRepository.AddCustomer(customer);
+            return data;
+
+
+        }
+
+        public async Task<ICollection<Customer>> GetAllCustomer()
+        {
+            return await _customerRepository.GetAllCustomer();
+        }
+
+        public async Task<Customer>GetCustomer(int id)
+        {
+            return await _customerRepository.GetCustomer(id);
+        }
+
+        public async Task<CustomerResDTO> UpdateCustomer(CustomerReqDTO customerReqDTO , int id)
+        {
+            var getCustomer = await _customerRepository.GetCustomer(id);
+
+            getCustomer.CustomerName = customerReqDTO.CustomerName;
+            getCustomer.Address = customerReqDTO.Address;
+            getCustomer.PhoneNumber = customerReqDTO.PhoneNumber;
+            getCustomer.EmailAddress = customerReqDTO.EmailAddress;
+            getCustomer.Age = customerReqDTO.Age;
+
+
+            var check = await _customerRepository.EditCustomer(getCustomer);
+
+            var res = new CustomerResDTO
+            {
+                CustomerId = id,
+                CustomerName = check.CustomerName,
+                Address = check.Address,
+                PhoneNumber = check.PhoneNumber,
+                EmailAddress = check.EmailAddress,
+                Age= check.Age,
+
+            };
+
+            return res;
+
+        }
+
+
+        public async Task<Customer> DeleteCustomer(int id)
+        {
+            return await _customerRepository.DeleteCustomer(id); 
+        }
+
+
+    }
+}
